b2b3b9485fba9bf632d56e4e4b4a1e841aeb857a
vgsb18 <vgsb18@inf.ufpr.br>
2019-10-11 15:25:35 -0300

    Começo

---
+++ main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+	**mapa;
+	geramapa(mapa);
+	return 0;
+}
---
+++ space.h
@@ -0,0 +1,15 @@
+#ifndef __SPACE__
+#define __SPACE__
+
+typedef struct mapa
+{   
+    char **data;
+    int linhas;
+    int colunas;
+} mapa;
+
+
+
+
+
+#endif
